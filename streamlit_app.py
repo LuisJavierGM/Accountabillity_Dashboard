2f0ce5cb6e42ccad65060b3619ecc4a162f067f8
Luis Javier Gutiérrez Márquez <luisjavier.gma@gmail.com>
2023-11-01 15:06:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,8 @@ from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 st.set_page_config(
     layout='wide', 
-    initial_sidebar_state='expanded'
+    initial_sidebar_state='expanded',
+    theme='light'
 )
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
