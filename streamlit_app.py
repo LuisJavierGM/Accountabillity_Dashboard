05125b3daaf66c25b0d99bdef4aba8bbb0d6b5e4
Luis Javier Gutiérrez Márquez <luisjavier.gma@gmail.com>
2023-11-01 13:32:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,7 +99,7 @@ if selected == 'Facturas':
                 barra=pd.DataFrame()
                 barra['Factura']=lista2
                 barra['Total']=lista1
-                fig=px.bar(barra,x='Factura',y='Total',color='Total',color_continuous_scale=['#22AA33','#6F6EFF','#2A3864'],width=200,height=200)
+                fig=px.bar(barra,x='Factura',y='Total',color='Total',color_continuous_scale=['#22AA33','#6F6EFF','#2A3864'],width=1000,height=1000)
                 fig.update_layout(title='Total Facturas')
                 st.write(fig)
             with Cascada:
